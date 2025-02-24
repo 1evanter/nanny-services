01a82a85cce12784c04c8c201d6b13232d746d72
1evanter <rita.oleksenkoo@gmail.com>
2025-02-24 19:40:34 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+"use client"
+import { useAuthState } from "react-firebase-hooks/auth";
+import {auth} from "@/app/firebase/config"
+import { HomePage } from "./components/HomePage/HomePage";
+
 export default function Home() {
+  const [user] = useAuthState(auth);
+  console.log({ user });
+
   return (
-  <>Home Page</>
+<HomePage/>
   );
 }
