d666d6d35f4eaf6f56bdabfc28d3abf96a3ede56
1evanter <rita.oleksenkoo@gmail.com>
2025-02-24 22:15:47 +0200

    Create Toolbar.tsx

---
+++ src/app/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,10 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+// import { auth } from "@/app/firebase/config"
+import { signOut } from "firebase/auth";
+
+export const Toolbar = () => {
+    //   const [user] = useAuthState(auth);
+    return (
+       <div>Toolbar</div>
+    )
+}
