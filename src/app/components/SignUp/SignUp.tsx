dae17b08366c8a5f6fab4fa3368a5f04cb58401b
1evanter <rita.oleksenkoo@gmail.com>
2025-02-24 17:15:37 +0200

    Create SignUp.tsx

---
+++ src/app/components/SignUp/SignUp.tsx
@@ -0,0 +1,58 @@
+"use client"
+import { useForm } from "react-hook-form"
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth"
+import {auth} from '@/app/firebase/config'
+import { useRouter } from "next/navigation"
+
+type Inputs = {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export const SignUp = () => {
+const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth)
+const router = useRouter();
+    
+const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    } = useForm<Inputs>();
+    
+    
+  const onSubmit = async ({email, password}: Inputs) => {
+    try {
+        const res = await createUserWithEmailAndPassword(email, password);
+        if (res) {
+          alert("Реєстрація успішна!");
+        console.log(res)
+     router.push("/");   
+        } else {
+            return (<p>pizdec</p>)
+        }
+       
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+
+  return (
+ 
+      <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Registration</h2>
+          <p>Thank you for your interest in our platform! In order to register, we need some information. Please provide us with the following information.</p>
+ 
+          <input {...register("name")} placeholder="Name" />
+      <input {...register("email")} placeholder="Email"/>
+      <input {...register("password")} placeholder="Password"/>
+
+      {errors.password && <span>This field is required</span>}
+
+      <button type="submit">
+        Sign Up
+      </button>
+    </form>
+  )
+}
