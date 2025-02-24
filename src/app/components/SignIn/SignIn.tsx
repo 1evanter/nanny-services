88cfd8bae2f1b339e968f20ad3ad391ea8685b7c
1evanter <rita.oleksenkoo@gmail.com>
2025-02-24 17:15:39 +0200

    Create SignIn.tsx

---
+++ src/app/components/SignIn/SignIn.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import {auth} from '@/app/firebase/config'
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useRouter } from "next/navigation";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+export const SignIn = () => {
+    const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth)
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit = async ({email, password}: Inputs) => {
+    try {
+        const res = await signInWithEmailAndPassword(email, password);
+        console.log(res)
+      alert("Вхід успішний!");
+      router.push("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div>
+          <h2>Log In</h2>
+          <p>Welcome back! Please enter your credentials to access your account and continue your babysitter search.</p>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} placeholder="Email"/>
+      <input {...register("password")} placeholder="Password"/>
+        {errors.password && <p>{errors.password.message}</p>}
+
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+}
