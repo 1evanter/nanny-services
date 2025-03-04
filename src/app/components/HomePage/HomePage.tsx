76c828f4f9d10fe892c320f40248275c1cefef3a
1evanter <rita.oleksenkoo@gmail.com>
2025-03-04 16:24:44 +0200

    HomePage basic styling

--- src/app/components/HomePage/HomePage.tsx
+++ src/app/components/HomePage/HomePage.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link"
+import Image from 'next/image';
+import styles from "./HomePage.module.css"
 
 export const HomePage = () => {
     return (
-        <div>   
-        <h2>Make Life Easier for the Family: <span>Find Babysitters Online for All Occasions</span></h2>
+        <div className={styles.wrapper}>
+        <div className={styles.titleWrapper}>   
+        <h2 className={styles.title}>Make Life Easier for the Family: <br/> <span className={styles.span}>Find Babysitters Online for All Occasions</span></h2>
         <Link href="/nannies">Get Started</Link>       
+            </div>
+            <Image src="/images/hero-image.jpg" alt="Woman with baby reading book" width={700} height={736} />
         </div>
+        
     )
 }
