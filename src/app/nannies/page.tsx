d487d356065a21f7fdeec0d452001a22de5cf386
1evanter <rita.oleksenkoo@gmail.com>
2025-03-05 19:49:22 +0200

    Update page.tsx

--- src/app/nannies/page.tsx
+++ src/app/nannies/page.tsx
@@ -1,5 +1,9 @@
-export default function Page () {
-    return (
-        <>Nannies</>
-    )
+import { Nanny } from "@/types/nannies.types";
+import { getNannies } from "../(server)/api"
+import { NanniesPage } from "../components/NanniesPage/NanniesPage";
+
+export default async function Page() {
+  const nannies: Nanny[] = await getNannies();
+
+  return <NanniesPage initialData={nannies} />;
 }
