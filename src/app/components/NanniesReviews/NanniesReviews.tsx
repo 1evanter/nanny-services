727c7976984dbea69fd6494d7b401b6ad09fd8e8
1evanter <rita.oleksenkoo@gmail.com>
2025-03-09 23:15:23 +0200

    NanniesReview styles

--- src/app/components/NanniesReviews/NanniesReviews.tsx
+++ src/app/components/NanniesReviews/NanniesReviews.tsx
@@ -1,4 +1,6 @@
 import { Review } from "@/types/nannies.types";
+import Image from "next/image";
+import styles from "./NanniesReview.module.css"
 
 type NanniesReviewsProps = {
   reviews: Review[];
@@ -6,13 +8,18 @@ type NanniesReviewsProps = {
 
 export const NanniesReviews = ({ reviews }: NanniesReviewsProps) => {
     return (
-        <ul>
+        <ul className={styles.list}>
             {reviews.map(({ comment, rating, reviewer }, index) => 
             (
               <li key={index}>   
-            <div>
-              {reviewer}            
-                {rating}
+                <div className={styles.infoContainer}>
+                   <div className={styles.profile}>{reviewer.slice(0, 1)}</div>
+                  <div> <p>{reviewer}</p> 
+                    <div className={styles.rating}>
+                      <Image src="/icons/star.svg" alt="star icon" width={16} height={16}/>  {rating}  
+                    </div>
+                  
+                </div>
             </div>   
           <p>{comment}</p>
         </li>
