bb06c0193ddff48af6696c164321b77881dc82e7
1evanter <rita.oleksenkoo@gmail.com>
2025-03-09 15:48:37 +0200

    nanniesCard styles

--- src/app/components/NanniesCard/NanniesCard.tsx
+++ src/app/components/NanniesCard/NanniesCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import {Nanny} from "../../../types/nannies.types"
 import { NanniesReviews } from "../NanniesReviews/NanniesReviews";
+import styles from "./NanniesCard.module.css"
 
 type NannyCardProps = {
   nanny: Nanny;
@@ -25,23 +26,29 @@ export const NanniesCard = ({nanny}: NannyCardProps) => {
 
     return (
         <>
-            <div>
-                  <Image
+            <div className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        className={styles.image}
           src={avatar_url}
           alt="Nanny photo"
-          width={40}
-          height={40}
-                />
-                <div>
-                    <p>Nanny</p>
-                    <div>
+          width={96}
+          height={96}
+                    />
+                </div>
+                <div
+                    // className={styles.infoContainer}
+                >
+                <div className={styles.basicInfoContainer}>
+                   <p className={styles.name}><span>Nanny</span> <br />{name}</p>
+                    <div className={styles.basicInfo}>
                         <p>{location}</p>
                         <p>Rating: {rating}</p>
                         <p>Price / 1 hour: {price_per_hour}</p>
                     </div>
                 </div>
-                    <p>{name}</p>
-                <div>
+                   
+                <div className={styles.mainInfo}>
                     <p>Age: {birthday}</p>
                     <p>Experience: {experience}</p>
                     <p>Kids age: {kids_age}</p>
@@ -49,7 +56,8 @@ export const NanniesCard = ({nanny}: NannyCardProps) => {
                     <p>Characters: {characters}</p>
                     <p>{about}</p>
                 </div>
-                <div>Reviews: {<NanniesReviews reviews={reviews} />}</div>
+                    <div>Reviews: {<NanniesReviews reviews={reviews} />}</div>
+                    </div>
      </div>
         </>
     )
