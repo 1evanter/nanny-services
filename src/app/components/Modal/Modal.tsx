71ab38963393176dd0190ae8de32ca8fd77eb5af
1evanter <rita.oleksenkoo@gmail.com>
2025-03-10 15:35:52 +0200

    Create Modal.tsx

---
+++ src/app/components/Modal/Modal.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import styles from "./Modal.module.css";
+
+type ModalProps = {
+  children: React.ReactNode;
+};
+
+export const Modal = ({ children }: ModalProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        router.back();
+      }
+    };
+    document.addEventListener("keydown", handleEscape);
+    return () => document.removeEventListener("keydown", handleEscape);
+  }, [router]);
+
+   return (
+        <div className={styles.overlay} onClick={() => router.back()}>
+            <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+                {children}
+                <button className={styles.close} onClick={() => router.back()}>
+                    <Image src="/icons/x.svg" alt="close modal" width={32} height={32} />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+
+    
+   
