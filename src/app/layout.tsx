c5d95df7274a8b844040ef8f6907850514d0d90c
1evanter <rita.oleksenkoo@gmail.com>
2025-03-10 15:38:33 +0200

    add modal into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,15 +12,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>
-        <Header/>
+        <Header />
         <main>
           {children}
+        {modal}
         </main>
       </body>
     </html>
