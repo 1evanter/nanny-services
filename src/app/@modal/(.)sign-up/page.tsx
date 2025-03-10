dde36da49c7007b63ec83686e94fbe694ff38692
1evanter <rita.oleksenkoo@gmail.com>
2025-03-10 14:53:17 +0200

    create @modal

---
+++ src/app/@modal/(.)sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { Modal } from '@/app/components/Modal/Modal';
+import { SignUp } from '@/app/components/SignUp/SignUp';
+
+export default function Page() {
+  return (
+    <Modal>
+      <SignUp />
+    </Modal>
+  );
+}
