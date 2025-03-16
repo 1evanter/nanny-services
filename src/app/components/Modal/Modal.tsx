900b378cbd54fcf093dcf7d5931c37bdd6b1106e
1evanter <rita.oleksenkoo@gmail.com>
2025-03-16 19:55:29 +0200

    Update Modal.tsx

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -7,28 +7,53 @@ import styles from "./Modal.module.css";
 
 type ModalProps = {
   children: React.ReactNode;
+  toggleModalOpen?: () => void;
+  isModalOpen?: boolean;
+  stayOnPage?: boolean;
 };
 
-export const Modal = ({ children }: ModalProps) => {
+export const Modal = ({ children, toggleModalOpen, isModalOpen, stayOnPage }: ModalProps) => {
   const router = useRouter();
 
+  const closeModal = () => {
+    if (toggleModalOpen) {
+    toggleModalOpen() 
+    } else {
+      router.back()
+  }
+  }
+
   useEffect(() => {
+    document.documentElement.style.overflow = 'hidden';
+    
     const handleEscape = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
-        router.back();
+        closeModal();
       }
     };
     document.addEventListener("keydown", handleEscape);
-    return () => document.removeEventListener("keydown", handleEscape);
-  }, [router]);
+    return () => {
+        document.documentElement.style.overflow = '';
+      document.removeEventListener("keydown", handleEscape)
+    }
+     
+  }, [isModalOpen]);
 
+   const handleClose = () => {
+        document.body.classList.remove("modal-open");
 
+        if (stayOnPage && toggleModalOpen) {
+            toggleModalOpen();
+        } else {
+            router.back();
+        }
+    };
 
    return (
-        <div className={styles.overlay} onClick={() => router.back()}>
+        <div className={styles.overlay} onClick={handleClose}>
             <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
                 {children}
-                <button className={styles.closeButton} onClick={() => router.back()}>
+                <button className={styles.closeButton} onClick={handleClose}>
                     <Image src="/icons/x.svg" alt="close modal" width={32} height={32} />
                 </button>
             </div>
