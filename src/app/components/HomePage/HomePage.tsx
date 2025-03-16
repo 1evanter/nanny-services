c9258394b8cd068b7bf2e80dbe78e34b4789992c
1evanter <rita.oleksenkoo@gmail.com>
2025-03-16 15:45:51 +0200

    add icon

--- src/app/components/HomePage/HomePage.tsx
+++ src/app/components/HomePage/HomePage.tsx
@@ -7,7 +7,7 @@ export const HomePage = () => {
         <div className={styles.wrapper}>
         <div className={styles.titleWrapper}>   
         <h2 className={styles.title}>Make Life Easier for the Family: <br/> <span className={styles.span}>Find Babysitters Online for All Occasions</span></h2>
-        <Link href="/nannies" className={styles.link}>Get Started</Link>       
+                <Link href="/nannies" className={styles.link}>Get Started <Image src='/icons/arrow.svg' alt="get started icon" width={18} height={18} /> </Link>       
             </div>
                 <Image src="/images/hero-image.jpg" alt="Woman with baby reading book" width={700} height={736} />               
         </div>
