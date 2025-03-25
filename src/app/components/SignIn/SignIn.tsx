6064c3d4bac7c113b19cfb0238f52442ad49b2dd
1evanter <rita.oleksenkoo@gmail.com>
2025-03-25 19:08:59 +0200

    add validation

--- src/app/components/SignIn/SignIn.tsx
+++ src/app/components/SignIn/SignIn.tsx
@@ -4,6 +4,8 @@ import { useForm } from "react-hook-form";
 import {auth} from '@/app/firebase/config'
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
+import { schema } from "@/schemes/signIn"
+import {yupResolver} from "@hookform/resolvers/yup"
 import styles from "./SignIn.module.css"
 
 type Inputs = {
@@ -14,17 +16,20 @@ type Inputs = {
 export const SignIn = () => {
     const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth)
   const router = useRouter();
-  const {
+ const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Inputs>();
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = async ({email, password}: Inputs) => {
     try {
         const res = await signInWithEmailAndPassword(email, password);
-        console.log(res)
+        if (res?.user) {
       router.push("/");
+    }
     } catch (error) {
       alert(error);
     }
@@ -35,9 +40,10 @@ export const SignIn = () => {
           <h2 className={styles.title}>Log In</h2>
           <p className={styles.subtitle}>Welcome back! Please enter your credentials to access your account and continue your babysitter search.</p>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("email")} placeholder="Email"/>
+        <input {...register("email")} placeholder="Email" />
+      {errors.email && <p className={styles.error}>{errors.email.message}</p>}
       <input {...register("password")} placeholder="Password"/>
-        {errors.password && <p>{errors.password.message}</p>}
+        {errors.password && <p className={styles.error}>{errors.password.message}</p>}
 
         <button className={styles.submit} type="submit">Log In</button>
       </form>
--- src/app/components/SignUp/SignUp.tsx
+++ src/app/components/SignUp/SignUp.tsx
@@ -3,6 +3,9 @@ import { useForm } from "react-hook-form"
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth"
 import {auth} from '@/app/firebase/config'
 import { useRouter } from "next/navigation"
+import { schema } from "@/schemes/signUp"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { updateProfile } from "firebase/auth";
 import styles from "./SignUp.module.css"
 
 type Inputs = {
@@ -15,20 +18,19 @@ export const SignUp = () => {
 const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth)
 const router = useRouter();
     
-const {
+ const {
     register,
     handleSubmit,
     formState: { errors },
-    } = useForm<Inputs>();
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
     
-    
-  const onSubmit = async ({email, password}: Inputs) => {
-    try {
-        const res = await createUserWithEmailAndPassword(email, password);  
-        console.log(res)
-     router.push("/");   
-    } catch (error) {
-      alert(error);
+  const onSubmit = async ({ name, email, password }: Inputs) => {
+    const res = await createUserWithEmailAndPassword(email, password);
+    if (res?.user) {
+      await updateProfile(res.user, { displayName: name });
+      router.push("/");
     }
   };
 
@@ -39,11 +41,12 @@ const {
     <p className={styles.subtitle}>Thank you for your interest in our platform! In order to register, we need some information. Please provide us with the following information.</p>
       <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
  
-          <input {...register("name")} placeholder="Name" />
-      <input {...register("email")} placeholder="Email"/>
-      <input {...register("password")} placeholder="Password"/>
-
-      {errors.password && <span>This field is required</span>}
+        <input {...register("name")} placeholder="Name" />
+        {errors.name && <p className={styles.error}>{errors.name.message}</p>}
+        <input {...register("email")} placeholder="Email" />
+        {errors.email && <p className={styles.error}>{errors.email.message}</p>}
+        <input {...register("password")} placeholder="Password" />
+           {errors.password && <p className={styles.error}>{errors.password.message}</p>}
 
       <button className={styles.submit} type="submit">
         Sign Up
