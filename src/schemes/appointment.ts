3809371620d379a1c9f7af1f0fbeb4aa87aa0ddf
1evanter <rita.oleksenkoo@gmail.com>
2025-03-25 20:34:51 +0200

    Update appointment.ts

--- src/schemes/appointment.ts
+++ src/schemes/appointment.ts
@@ -2,9 +2,12 @@ import * as yup from 'yup';
 
 const regExpPhone = new RegExp(/^\+380[3-9][0-9]{8}$/);
 const regExpTime = new RegExp(/^([01]\d|2[0-3]):([0-5]\d)$/);
+const regExpAge = new RegExp(/^\S[0-9]{0,3}$/);
+
 
 export const schema = yup.object().shape({
     address: yup.string().trim().required('Enter your address'),
+    age: yup.string().matches(regExpAge, 'Invalid Age').required("Enter your child's age"),
   name: yup.string().min(2).trim().required('Enter your name'),
   phone: yup
     .string()
